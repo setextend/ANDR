20a7ecaf32ba19813fb233a77cbd89c207d4630c
Sergey Smirnov <setextend@gmail.com>
2021-04-26 11:12:23 +0300

    added repository & viewmodel

---
+++ app/src/main/java/ru/netology/repository/PostRepository.kt
@@ -0,0 +1,9 @@
+package ru.netology.repository
+
+import androidx.lifecycle.LiveData
+import ru.netology.Post
+
+interface PostRepository {
+    fun get(): LiveData<Post>
+    fun like()
+}
---
+++ app/src/main/java/ru/netology/viewmodel/PostViewModel.kt
@@ -0,0 +1,11 @@
+package ru.netology.viewmodel
+
+import androidx.lifecycle.ViewModel
+import ru.netology.repository.PostRepository
+import ru.netology.repository.PostRepositoryInMemory
+
+class PostViewModel : ViewModel() {
+    private val repository: PostRepository = PostRepositoryInMemory()
+    val data = repository.get()
+    fun like() = repository.like()
+}
