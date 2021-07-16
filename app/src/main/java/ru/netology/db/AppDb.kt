4475db286e39448fe10a1f05f09c0854886fbf5b
Sergey Smirnov <setextend@gmail.com>
2021-07-16 10:36:02 +0300

    added AppDb.kt

--- app/src/main/java/ru/netology/db/AppDb.kt
+++ app/src/main/java/ru/netology/db/AppDb.kt
@@ -1,48 +1,29 @@
 package ru.netology.db
 
 import android.content.Context
-import android.database.sqlite.SQLiteDatabase
-import android.database.sqlite.SQLiteOpenHelper
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import ru.netology.dao.PostDao
-import ru.netology.dao.PostDaoImpl
+import ru.netology.entity.PostEntity
 
-class AppDb private constructor (db:SQLiteDatabase){
-    val postDao: PostDao = PostDaoImpl(db)
+@Database(entities = [PostEntity::class], version = 1)
+abstract class AppDB : RoomDatabase() {
+    abstract fun postDao() : PostDao
 
     companion object {
         @Volatile
-        private var instance: AppDb? = null
+        private var instance : AppDB? = null
 
-        fun getInstance(context: Context): AppDb {
-            return instance?: synchronized(this) {
-                instance?: AppDb(buildDatabase(context, arrayOf(PostDaoImpl.DDL)))
+        fun getInstance(context: Context) : AppDB {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it}
             }
         }
 
-        private fun buildDatabase(context: Context, DDLs: Array<String>) = DbHelper(
-            context, 3, "app.db", DDLs,
-        ).writableDatabase
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context, AppDB::class.java,"app.db")
+                .allowMainThreadQueries()
+                .build()
     }
-}
-
-class DbHelper(context: Context, dbVersion: Int, dbName: String, private val DDLs: Array<String>) :
-    SQLiteOpenHelper(context, dbName, null, dbVersion) {
-
-    override fun onCreate(db: SQLiteDatabase) {
-        DDLs.forEach {
-            db.execSQL(it)
-        }
-    }
-
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        db.execSQL("DROP TABLE IF EXISTS posts")
-        DDLs.forEach {
-            db.execSQL(it)
-        }
-    }
-
-    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        // TODO("Not yet implemented")
-    }
-
 }
